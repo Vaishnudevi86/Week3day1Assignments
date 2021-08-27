d7d2005b11ff1078656eddf13c38f4a62bc4ccd6
Vaishnudevi86 <88608139+Vaishnudevi86@users.noreply.github.com>
2021-08-28 01:13:58 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,11 @@
+package org.department;
+
+import org.college.College;
+
+
+public class Department extends College {
+	public void DeptName() {
+		System.out.println("Department name is ECE");
+	}
+
+}
