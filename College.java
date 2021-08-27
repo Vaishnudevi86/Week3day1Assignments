5f7683dcdfab344247baea17520c49b6ccf43423
Vaishnudevi86 <88608139+Vaishnudevi86@users.noreply.github.com>
2021-08-28 01:13:44 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("College name is BCET");
+	}
+
+	public void collegeCode() {
+		System.out.println("College code is 1245");
+	}
+
+	public void collegeRank() {
+		System.out.println("College rank is First");
+	}
+
+}
